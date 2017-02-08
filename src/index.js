716581829d9afecefce1c25d3386d1610939a4e6
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-07 20:55:17 -0800

    imported React code to handle Router, render React components on index.html; imported css;

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+import 'babel-polyfill'; // needed to polyfill ES6 that babel can't transpile -- rather big (50k), so should actually only import the bits of it that are needed, but quick and dirty for the tutorial
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';  // gives nice, clean urls; could use hashRouter, but a bit ugly looking
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+	<Router history={browserHistory} routes={routes} />,
+	document.getElementById('app')
+);
