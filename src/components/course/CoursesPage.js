2b866d1fbfb8289c107c4c6839bb492fc1e1a2a5
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-09 22:59:01 -0800

    added constructor for props, context and state (with course.title) and a form to create a course

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,30 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			course: { title: null }
+		};
+	}
 	render() {
 		return (
 			<div>
 				<h1>Courses</h1>
+				<input 
+					type="text"
+					onChange={this.onTitleChange}
+					value={this.state.course.title}
+				/>
+				<input 
+					type="submit"
+					value="Save"
+					onClick={this.onClickSave}
+				/>
+
 			</div>
 		);
 	}
-};
+}
 
 export default CoursesPage;
