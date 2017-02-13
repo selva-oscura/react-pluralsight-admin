eb317a7950d16ef768da721f884127dc8d5cc282
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-12 20:27:30 -0800

    imported CourseList, created courses const, pass course data to and display CourseList Component

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
 	constructor(props, context) {
@@ -11,10 +12,11 @@ class CoursesPage extends React.Component {
 		return <div key={index}>{course.title}</div>;
 	}
 	render() {
+		const {courses} = this.props;
 		return (
 			<div>
 				<h1>Courses</h1>
-				{this.props.courses.map(this.courseRow)}
+				<CourseList courses={courses} />
 			</div>
 		);
 	}
