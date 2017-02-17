5b52f2ff42c38efc6a9a0db95c612a2d5dee30d1
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-16 22:14:55 -0800

    created authorReducer with LOAD_AUTHORS_SUCCESS

---
+++ src/reducers/authorReducer.js
@@ -0,0 +1,16 @@
+import * as types from '../actions/actionTypes';
+
+export default function authorReducer(state = [], action) {
+	switch (action.type) {
+		// case types.CREATE_AUTHOR:
+		// 	return [...state, 
+		// 		Object.assign({}, action.author)
+		// 	];
+
+		case types.LOAD_AUTHORS_SUCCESS:
+			return action.authors;
+
+		default: 
+			return state;
+	}
+}
