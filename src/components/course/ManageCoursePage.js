cd5b37262421806677818a68a01acaf9e9726586
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-16 21:57:31 -0800

    imported CourseForm; added course and errors to state; created dummy data for course; displayed CourseForm with dummy data;

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,25 +2,39 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
 	constructor (props, context) {
 		super(props, context);
+		this.state = {
+			course: Object.assign({}, props.course),
+			errors: {}
+		};
 	}
 	render() {
 		return (
-			<h1>Manage Course</h1>
+			<div>
+				<h1>Manage Course</h1>
+				<CourseForm 
+					course={this.state.course}
+					allAuthors={[]}
+					errors={this.state.errors}
+				/>
+			</div>
 		);
 	}
 }
 
-ManageCoursePage.PropTypes = {
-	//myProp: PropTypes.string.isRequired
+ManageCoursePage.propTypes = {
+	course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+	// dummy data for dev purposes
+	let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
 	return {
-		state: state
+		course: course
 	};
 }
 
