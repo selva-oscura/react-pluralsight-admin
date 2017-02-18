c8b918c0d859912125775fc1d1661992c881fdeb
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-17 22:02:45 -0800

    delay calling of getCourseById until after the API call for courses is complete (when state.courses.length > 0)

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -64,7 +64,7 @@ function getCourseById(courses, id) {
 function mapStateToProps(state, ownProps) {
 	const courseId = ownProps.params.id;
 	let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
-	if (courseId) {
+	if (courseId && state.courses.length > 0) {
 		course = getCourseById(state.courses, courseId);
 	}
 	const authorsFormattedForDropdown = state.authors.map(author => {
