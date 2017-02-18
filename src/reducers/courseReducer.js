8c6ef888b8303c6872fb2c6b0639e44f8e71990a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-18 15:52:05 -0800

    removed deprecated code

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,11 +3,6 @@ import initialState from './initialState';
 
 export default function courseReducer(state = initialState.courses, action) {
 	switch (action.type) {
-		// case types.CREATE_COURSE:
-		// 	return [...state, 
-		// 		Object.assign({}, action.course)
-		// 	];
-
 		case types.LOAD_COURSES_SUCCESS:
 			return action.courses;
 
