5c8f2d1d7f4f7f33e603cadf63c13a358e3ea7ba
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-18 18:12:24 -0800

    imported toastr css

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {loadCourses} from './actions/courseActions';
 import {loadAuthors} from './actions/authorActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 
 // to pass initial state, primed from a server, database, or localStore, we'd pass this data to configureStore;
 const store = configureStore();
