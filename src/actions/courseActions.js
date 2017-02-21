ad0a972b289417b0e571554b3b3a5344d21cb076
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-20 22:04:29 -0800

    added error handling for loadCourses and saveCourse with call to dispatch(ajaxCallError())

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 // export function createCourse(course) {
 // 	return { type: types.CREATE_COURSE, course };
@@ -24,6 +24,7 @@ export function loadCourses() {
 		return courseApi.getAllCourses().then(courses => {
 			dispatch(loadCoursesSuccess(courses));
 		}).catch(error => {
+			dispatch(ajaxCallError());
 			throw error;
 		});
 	};
@@ -37,7 +38,8 @@ export function saveCourse(course) {
 			course.id ? dispatch(updateCourseSuccess(course)) :
 				dispatch(createCourseSuccess(course));
 		}).catch(error => {
-			throw(error);
+			dispatch(ajaxCallError());
+			throw error;
 		});
 	};
 }
