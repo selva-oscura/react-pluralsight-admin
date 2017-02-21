6eb930d6836da43f39ae95037243f7be77c5b14f
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-20 23:06:53 -0800

    basic set-up and first test (presence of form) for CourseForm component

---
+++ src/components/course/CourseForm.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import expect from 'expect';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+function setup() {
+	let props = {
+		course: {}, saving: false, errors: {},
+		onSave: () => {},
+		onChange: () => {}
+	};
+
+	let renderer = TestUtils.createRenderer();
+	renderer.render(<CourseForm {...props}/>);
+	let output = renderer.getRenderOutput();
+	return {
+		props, 
+		output,
+		renderer
+	};
+}
+
+describe('CourseForm via React Test Utils', () => {
+	it('renders form', () => {
+		const { output } = setup();
+		expect(output.type).toBe('form');
+	});
+});
