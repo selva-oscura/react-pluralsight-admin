1d05395409695a89ea3e4a4d8db465ef7735be6a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-21 22:34:31 -0800

    imported ManageCoursePage class; added props, defined wrapper and passed props to ManageCoursePage in its mount; found saveButton in wrapper, checked prop of saveButton, and simulated click with a too-short/empty title;

--- src/components/course/ManageCoursePage.Enzyme.test.js
+++ src/components/course/ManageCoursePage.Enzyme.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import expect from 'expect';
 import {mount, shallow} from 'enzyme';
-import ManageCoursePage from './ManageCoursePage';
+import {ManageCoursePage} from './ManageCoursePage';
 
 describe('ManageCoursePage via Enzyme', () => {
 	it('sets error message when trying to save empty title', () => {
-		const wrapper=mount(<ManageCoursePage />);
+		// define props to be passed
+		const props = {
+			authors: [],
+			actions: { saveCourse: () => {return Promise.resolve(); }},
+			course: {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
+		};
+		// define wrapper
+		const wrapper=mount(<ManageCoursePage {...props} />);
+
+		// define saveButton
+		const saveButton = wrapper.find('input').last();
+
+		// check type of saveButton
+		expect(saveButton.prop('type')).toBe('submit');
+
+		// simulate click
+		saveButton.simulate('click');
+		expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
 	});
 });
