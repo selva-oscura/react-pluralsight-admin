50a7a327ab3dba896a555aba4b3be8b73355b282
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-21 19:21:46 -0800

    added test (using Enzyme) for save button being labeled Save when saving is false

--- src/components/course/CourseForm.Enzyme.test.js
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -18,4 +18,14 @@ describe('CourseForm via Enzyme', () => {
 		expect(wrapper.find('form').length).toBe(1);
 	});
 
+	it('save button is labeled Save when not saving', () => {
+		const wrapper = setup(false);
+		expect(wrapper.find('input').props().value).toBe('Save');
+	});
+
+	it('save button is labeled Saving... when saving', () => {
+		const wrapper = setup(true);
+		expect(wrapper.find('input').props().value).toBe('Saving....');
+	});
+
 });
