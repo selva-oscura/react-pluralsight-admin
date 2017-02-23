5e025e4c386174c2ab46c7756f5f90203441ac25
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-22 18:42:30 -0800

    basic set-up for testing courseActions

---
+++ src/actions/courseActions.Enzyme.test.js
@@ -0,0 +1,12 @@
+import expect from 'expect';
+import * as courseActions  from './courseActions';
+import * as types from './actionTypes';
+
+// Test a sync action
+describe('Course Actions', () => {
+	describe('createCourseSuccess', () => {
+		it('should create a CREATE_COURSE_SUCCESS action', () => {
+
+		})
+	});
+});
