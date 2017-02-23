428063c585715ea92d1edc41e0b08ba41c41109e
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-22 18:46:24 -0800

    tested that calling createCourseAction producers the correct action output

--- src/actions/courseActions.Enzyme.test.js
+++ src/actions/courseActions.Enzyme.test.js
@@ -6,7 +6,17 @@ import * as types from './actionTypes';
 describe('Course Actions', () => {
 	describe('createCourseSuccess', () => {
 		it('should create a CREATE_COURSE_SUCCESS action', () => {
-
-		})
+			// course information
+			const course = {id: 'clean-code', title: 'Clean Code'};
+			// expected output
+			const expectedAction = {
+				type: types.CREATE_COURSE_SUCCESS,
+				course: course
+			};
+			// call to create action
+			const action = courseActions.createCourseSuccess(course);
+			// assert
+			expect(action).toEqual(expectedAction);
+		});
 	});
 });
