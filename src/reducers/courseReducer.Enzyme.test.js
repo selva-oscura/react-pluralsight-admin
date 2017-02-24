98dc2de81219626bbc124847f68121c5d0196530
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-24 15:24:46 -0800

    tested UPDATE_COURSE_SUCCESS that number of courses after test is as expected;

--- src/reducers/courseReducer.Enzyme.test.js
+++ src/reducers/courseReducer.Enzyme.test.js
@@ -17,4 +17,18 @@ describe('Course Reducer', () => {
 		expect(newState[2].title).toEqual('C');
 	});
 
+	it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
+		const initialState = [
+			{id: 'A', title: 'A'},
+			{id: 'B', title: 'B'},
+			{id: 'C', title: 'C'},
+		];
+
+		const updatingCourse = {id: 'B', title: 'B2: Return of the B'};
+		const action = actions.updateCourseSuccess(updatingCourse);
+
+		const newState = courseReducer(initialState, action);
+		expect(newState.length).toEqual(3);
+	});
+
 });
