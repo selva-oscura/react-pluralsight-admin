b6afcccc0334a8745e023cbbde0fb35962bdcb79
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-24 23:59:40 -0800

    added descriptions to separate CREATE_COURSE_SUCC ESS and UPDATE_COURSE_SUCCESS

--- src/reducers/courseReducer.Enzyme.test.js
+++ src/reducers/courseReducer.Enzyme.test.js
@@ -3,40 +3,43 @@ import courseReducer from './courseReducer';
 import * as actions from '../actions/courseActions';
 
 describe('Course Reducer', () => {
-	it('should add course when passed CREATE_COURSE_SUCCESS', () => {
-		const initialState = [
-			{title: 'A'},
-			{title: 'B'}
-		];
-		const newCourse = {title: 'C'};
-		const action = actions.createCourseSuccess(newCourse);
-		const newState = courseReducer(initialState, action);
-		expect(newState.length).toEqual(3);
-		expect(newState[0].title).toEqual('A');
-		expect(newState[1].title).toEqual('B');
-		expect(newState[2].title).toEqual('C');
+	describe('CREATE_COURSE_SUCCESS', () => {
+		it('should add course when passed CREATE_COURSE_SUCCESS', () => {
+			const initialState = [
+				{title: 'A'},
+				{title: 'B'}
+			];
+			const newCourse = {title: 'C'};
+			const action = actions.createCourseSuccess(newCourse);
+			const newState = courseReducer(initialState, action);
+			expect(newState.length).toEqual(3);
+			expect(newState[0].title).toEqual('A');
+			expect(newState[1].title).toEqual('B');
+			expect(newState[2].title).toEqual('C');
+		});		
 	});
 
-	it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
-		const initialState = [
-			{id: 'A', title: 'A'},
-			{id: 'B', title: 'B'},
-			{id: 'C', title: 'C'},
-		];
+	describe('UPDATE_COURSE_SUCCESS', () => {
+		it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
+			const initialState = [
+				{id: 'A', title: 'A'},
+				{id: 'B', title: 'B'},
+				{id: 'C', title: 'C'},
+			];
 
-		const updatedTitle = 'B2: Return of the B';
-		const updatingCourse = {id: 'B', title: updatedTitle};
-		const action = actions.updateCourseSuccess(updatingCourse);
+			const updatedTitle = 'B2: Return of the B';
+			const updatingCourse = {id: 'B', title: updatedTitle};
+			const action = actions.updateCourseSuccess(updatingCourse);
 
-		const newState = courseReducer(initialState, action);
-		expect(newState.length).toEqual(3);
+			const newState = courseReducer(initialState, action);
+			expect(newState.length).toEqual(3);
 
-		const updatedCourse = newState.find(a => a.id === updatingCourse.id);
-		const untouchedCourse = newState.find(a => a.id === 'A');
+			const updatedCourse = newState.find(a => a.id === updatingCourse.id);
+			const untouchedCourse = newState.find(a => a.id === 'A');
 
-		expect(updatedCourse.title).toEqual(updatedTitle);
-		expect(untouchedCourse.title).toEqual('A');
+			expect(updatedCourse.title).toEqual(updatedTitle);
+			expect(untouchedCourse.title).toEqual('A');
 
+		});
 	});
-
 });
